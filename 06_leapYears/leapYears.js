5bb13379a1b90cd32e3b4bbeafcd377e40739a10
Firewill <Teo.Kohan@gmail.com>
2021-11-18 02:18:15 -0300

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = function(y) {
+    if (typeof y != "number")
+        return 'ERROR';
+    return !!(!(y % 400) || (!(y % 4) && y % 100));
 };
 
 // Do not edit below this line
