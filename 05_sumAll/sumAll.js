ab1d5a6055b2c85b2325c424b0ad1fb68f50a4fb
Firewill <Teo.Kohan@gmail.com>
2021-11-18 02:07:34 -0300

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number" || a < 0 || b < 0)
+        return 'ERROR';
+    if (a > b)
+        [a, b] = [b, a];
+    return (b*(b+1))/2 - ((a-1)*(a))/2;
 };
 
 // Do not edit below this line
