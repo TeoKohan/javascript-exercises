ac8f9c778b1bf39db30319673ce7c043fe7cc8ab
Firewill <Teo.Kohan@gmail.com>
2021-11-18 02:30:22 -0300

    Complete exercise 7

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(A) {
+  let res = 0;
+  for (let i = 0; i < A.length; ++i)
+    res += A[i];
+	return res;
 };
 
-const multiply = function() {
-
+const multiply = function(A) {
+  let res = 1;
+  for (let i = 0; i < A.length; ++i)
+    res *= A[i];
+	return res;
 };
 
-const power = function() {
-	
+const power = function(a, e) {
+  let res = 1;
+	for (let i = 0; i < e; ++i)
+    res *= a;
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let res = 1;
+  for (let i = 1; i <= n; ++i)
+    res *= i;
+  return res;
 };
 
 // Do not edit below this line
