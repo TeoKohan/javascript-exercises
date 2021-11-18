205858beff782bae23fbc2a1062477c4b293e365
Firewill <Teo.Kohan@gmail.com>
2021-11-18 02:00:02 -0300

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(A, es) {
+    let res = [];
+    for (let i = 0; i < A.length; ++i) {
+        let j = 1;
+        while (j < arguments.length && A[i] !== arguments[j])
+            ++j;
+        if (j == arguments.length)
+            res.push(A[i]);
+    }
+    return res;
 };
 
 // Do not edit below this line
