a4d318dce2db4db838f20e2a97f962e4999bbce6
Firewill <Teo.Kohan@gmail.com>
2021-11-21 11:32:57 -0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(A) {
+    return A.reduce((oldest, item) => {
+        if (getAge(item) > getAge(oldest))
+            return item;
+        return oldest;
+    }, A[0])
+};
 
+const getAge = function(o) {
+    return Math.abs((o.yearOfDeath ?? new Date().getFullYear()) - o.yearOfBirth);
 };
 
 // Do not edit below this line
