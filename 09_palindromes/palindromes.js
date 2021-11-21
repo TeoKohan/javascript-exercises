64496fb59589ba8cd683bef626527f4af0fe0150
Firewill <Teo.Kohan@gmail.com>
2021-11-21 10:43:28 -0300

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (s) {
+    s = s.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"");
+    let reverse = '';
+    for (let i = s.length - 1; i >= 0; --i)
+        reverse += s[i];
 
+    console.log(s, "-", reverse);
+    return s === reverse;
+    
 };
 
 // Do not edit below this line
